3523577df1d0b40396e8a671df4bb03dc23a03c3
Yasir Khairul Malik <yasirkm3071@gmail.com>
2023-06-10 22:07:48 +0700

    updated img_eval handler

--- handlers/skin_evaluation.py
+++ handlers/skin_evaluation.py
@@ -1,10 +1,35 @@
-from models.skin_model import model
+import base64
+from io import BytesIO
+
+import numpy as np
+from PIL import Image
+
+from tensorflow import keras
+
+from models.skin_model import model, CONDITIONS, RESOLUTION
 
 def img_eval(body):
+    # Process body of base64-encoded image to np array
+    skin_image = base64.b64decode(body)
+    skin_image = BytesIO(skin_image)
+    skin_image = Image.open(skin_image)
+    skin_image = skin_image.resize(RESOLUTION)
+
+    skin_image = keras.preprocessing.image.img_to_array(skin_image)
+    skin_image = np.expand_dims(skin_image, axis=0)
     # Do prediction
-    prediction = model.predict(body)
+    prediction = model.predict(skin_image)
+    prediction = prediction.astype('float64')
+    prediction_idx = np.argmax(prediction)
+    condition = CONDITIONS[prediction_idx]
+    confidence = prediction[0][prediction_idx]
 
     # Process the result
-    result = prediction
+    result = {
+        'condition' : condition,
+        'confidence' : confidence,
+    }
+
+    print(result)
 
     return result
