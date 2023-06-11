6fca46d61196fab45a931fce8faa2ff9d38d77bb
Yasir Khairul Malik <yasirkm3071@gmail.com>
2023-06-11 15:25:18 +0700

    Updated folder download

--- ml_models/skin_model.py
+++ ml_models/skin_model.py
@@ -9,11 +9,11 @@ import dermis_utils
 
 
 BASE_DIR = Path(__file__).parent
-MODEL_FOLDER = 'skinv1' #os.environ['MODEL']
+MODEL_FOLDER = os.environ['MODEL'] #'skinv1' 
 MODEL_FILE = 'model.h5'
 CONFIG_FILE = 'model.conf'
-BUCKET_NAME = 'dts-model' #os.environ('BUCKET')
-BUCKET_SUBDIR = Path('/')
+BUCKET_NAME = os.environ['BUCKET'] # 'dts-model'
+BUCKET_SUBDIR = Path('ml_models')
 LOCAL_SUBDIR = BASE_DIR
 KEY_PATH = BASE_DIR / 'auth' / 'service.json'
 
@@ -23,17 +23,17 @@ KEY_PATH = BASE_DIR / 'auth' / 'service.json'
 def download_folder(key_path, bucket_name, folder_path, save_path):
     client = storage.Client.from_service_account_json(key_path)
     bucket = client.get_bucket(bucket_name)
-    blobs = bucket.list_blobs()
+    blobs = bucket.list_blobs(prefix=folder_path.as_posix())
 
     for blob in blobs:
-        print(blob.name)
-        blob.download_to_filename(save_path/blob.name)
+        if not blob.name.endswith('/'):
+            blob.download_to_filename(save_path/Path(blob.name).name)
 
 model_folder = BUCKET_SUBDIR / MODEL_FOLDER
 save_folder = LOCAL_SUBDIR / MODEL_FOLDER
 save_folder.mkdir(exist_ok=True)
 
-download_folder(KEY_PATH, BUCKET_NAME, model_folder, save_folder.parent)
+download_folder(KEY_PATH, BUCKET_NAME, model_folder, save_folder)
 
 
 # download_blob(KEY_PATH, BUCKET, MODEL_PATH_CLOUD, MODEL_PATH_LOCAL)
