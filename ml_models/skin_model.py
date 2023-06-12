6956728fec8160064e2f0d286afc3e7b3758a5c2
Yasir Khairul Malik <yasirkm3071@gmail.com>
2023-06-12 19:18:45 +0700

    updated to enable cloud run deployment

--- ml_models/skin_model.py
+++ ml_models/skin_model.py
@@ -9,19 +9,19 @@ import dermis_utils
 
 
 BASE_DIR = Path(__file__).parent
-MODEL_FOLDER = os.environ['MODEL'] #'skinv1' 
+MODEL_FOLDER = os.environ['MODEL']
 MODEL_FILE = 'model.h5'
 CONFIG_FILE = 'model.conf'
-BUCKET_NAME = os.environ['BUCKET'] # 'dts-model'
+BUCKET_NAME = os.environ['BUCKET']
 BUCKET_SUBDIR = Path('ml_models')
 LOCAL_SUBDIR = BASE_DIR
-KEY_PATH = BASE_DIR / 'auth' / 'service.json'
+KEY_PATH = None if os.environ.get('ENVIRONMENT') == 'DEPLOYMENT' else BASE_DIR / 'auth' / 'service.json'
 
-# CONDITIONS = ["Actinic Keratosis", "Basal Cell Carcinoma", "Benign Keratosis", "Dermatofibroma", "Melanoma", "Melanocytic Nevus", "Squamous cell carcinoma", "Vascular Lesion"]
-# RESOLUTION = (240,240)
-
-def download_folder(key_path, bucket_name, folder_path, save_path):
-    client = storage.Client.from_service_account_json(key_path)
+def download_folder(bucket_name, folder_path, save_path, key_path=None):
+    if key_path:
+        client = storage.Client.from_service_account_json(key_path)
+    else:
+        client = storage.Client()
     bucket = client.get_bucket(bucket_name)
     blobs = bucket.list_blobs(prefix=folder_path.as_posix())
 
@@ -33,7 +33,7 @@ model_folder = BUCKET_SUBDIR / MODEL_FOLDER
 save_folder = LOCAL_SUBDIR / MODEL_FOLDER
 save_folder.mkdir(exist_ok=True)
 
-download_folder(KEY_PATH, BUCKET_NAME, model_folder, save_folder)
+download_folder(BUCKET_NAME, model_folder, save_folder, KEY_PATH)
 
 
 # download_blob(KEY_PATH, BUCKET, MODEL_PATH_CLOUD, MODEL_PATH_LOCAL)
