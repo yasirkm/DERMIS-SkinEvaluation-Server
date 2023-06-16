3b7ecfedcc422f9dbdf6c45d4388a1cd3523e6c3
Yasir Khairul Malik <yasirkm3071@gmail.com>
2023-06-16 19:15:35 +0700

    updated API spec and doc path

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import connexion
 
-app = connexion.FlaskApp(__name__, specification_dir='api/')
+options = {'swagger_url': '/'}
+
+app = connexion.FlaskApp(__name__, specification_dir='api/', options=optinos)
 app.add_api('openapi.yaml')
 app.run(port=8080)
