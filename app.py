4e710cc879a502454b39fd069a5daeeaf0c60288
Yasir Khairul Malik <yasirkm3071@gmail.com>
2023-06-16 20:07:22 +0700

    added error handling for invalid image encoding

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import connexion
 
+from PIL import UnidentifiedImageError
+
+from handlers.error import invalid_image_encoding
+
 options = {'swagger_url': '/'}
 
 app = connexion.FlaskApp(__name__, specification_dir='api/', options=options)
 app.add_api('openapi.yaml')
+app.add_error_handler(UnidentifiedImageError, invalid_image_encoding)
 
 app.run(port=8080)
---
+++ handlers/error.py
@@ -0,0 +1,2 @@
+def invalid_image_encoding(error):
+    return "Invalid image given. Please make sure the image is base64 encoded", 400
